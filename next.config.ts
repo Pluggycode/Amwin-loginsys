dfceee2c0499644fef7415a1ab449ffef23d2fe7
shashidhar k <160392735+Pluggycode@users.noreply.github.com>
2026-04-20 10:28:49 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,14 +6,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-const nextConfig = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete 
-    // even if your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-}
-
 export default nextConfig;
